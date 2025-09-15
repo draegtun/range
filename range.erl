7503519df104d3243d34edda436475c09e3986f6
Barry Walsh <draegtun@googlemail.com>
2025-09-15 17:02:14 +0100

    Big bang

---
+++ range.erl
@@ -0,0 +1,117 @@
+-module(range).
+-export [seq/2, seq/3, generate/2, wrap/2, next/1].
+-export [upto/1, countdown/1, forever/0, forever/1].
+-export [cycle/1].
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Core seq/next funcs
+
+seq(From, To) ->
+	seq(From, To, 1).
+
+% seq can be integers or floats
+seq(From, To, Step) when is_number(From), is_number(To), is_number(Step) ->
+	if 
+		Step > 0, From < To ->
+			{up, From, To, Step};
+		Step < 0, From > To ->
+			{down, From, To, Step};
+		true ->
+			throw(range_seq_invalid)
+	end.
+
+% function! Could return anything but N must be integer
+generate(Fun, N) when is_function(Fun), is_integer(N) ->
+	{function, Fun, N}.
+
+% wrap! Used to wrap functions with Seq tuple
+wrap(Fun, Seq) when is_function(Fun), is_tuple(Seq) ->
+	{wrap, Fun, Seq}.
+
+% next iterator
+next({up, From, To, Step}) when From =< To ->
+	Next = {up, From+Step, To, Step},
+	{From, Next};
+next({down, From, To, Step}) when From >= To ->
+	Next = {down, From+Step, To, Step},
+	{From, Next};
+next({forever, N}) ->
+	Next = {forever, N+1},
+	{N, Next};
+next({function, Fun, N}) ->
+	Next = Fun(N),
+	{N, {function, Fun, Next}};
+next({wrap, Fun, Seq}) ->
+	{N, Next} = Fun(Seq),
+	{N, {wrap, Fun, Next}};
+next(done) ->
+	% shouldn't happen but if someone does send 'done' then throw this error!
+	throw(range_seq_exhausted);
+next(_) ->
+	done.
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% seq/next nicety (seq builder) funcs 
+%% NB. These just return Seq{} because we cannot provide invalid Seq
+%%     Haven't put in fallback (throw error) clauses for these
+
+% upto(10) -> 0..9
+upto(N) when is_integer(N) andalso N > 0 ->
+	seq(0, N-1).
+
+% countdown(10) -> 10..1
+countdown(N) when is_integer(N) andalso N > 0 ->
+	seq(N, 1, -1).
+
+% forever() -> 1..infinity
+forever() ->
+	forever(1).
+
+forever(From) when is_integer(From) ->
+	{forever, From}.
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% wrapped funcs
+
+% cycle a (fixed) Seq
+cycle({up, _, _, _} = Seq) ->
+	cycle2(Seq);
+cycle({down, _, _, _} = Seq) ->
+	cycle2(Seq);
+cycle(_) ->
+	throw(range_cycle_invalid).
+
+cycle2(Seq0) ->
+	Fun = fun(Seq) ->
+		case range:next(Seq) of
+			done -> 
+				range:next(Seq0);  	%reset, ie. recycle
+			Range ->
+				Range				% continue with current Seq
+		end
+	end,
+	wrap(Fun, Seq0).
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% What things from lists: module??
+% Whats applicable?  whats important??  don't want too much flaff/fluff :)
+
+% cycle / filter
+% map / foreach
+% length? (cant do on forever/func-gen)
+% foldl foldr
+% [DONE] forever (from 1)
+% function generate?  (means you could return otherthings to numbers?)
+% take? (is this needed for working with forever and func-gen?)
+% from_list ?   (Nope, if got a list then just use lists:* functions
+% to_list
+% member (is N in range)
+
+% not an ITERATOR, just RANGE (of numbers), so don't need everything!
+
---
+++ range_tests.erl
@@ -0,0 +1,100 @@
+-module(range_tests).
+-include_lib("eunit/include/eunit.hrl").
+
+seq_invalid_test() ->
+    ?assertThrow(range_seq_invalid, range:seq(4,4)),
+    ?assertThrow(range_seq_invalid, range:seq(4,3)),
+    ?assertThrow(range_seq_invalid, range:seq(3,4,-1)).
+
+seq_up_test() ->
+    Seq = range:seq(1,3),
+    {1, N1} = range:next(Seq),
+    ?assertEqual({up, 2, 3, 1}, N1),
+    {2, N2} = range:next(N1),
+    ?assertEqual({up, 3, 3, 1}, N2),
+    {3, N3} = range:next(N2),
+    ?assertEqual({up, 4, 3, 1}, N3),
+    ?assertEqual(done, range:next(N3)).
+
+seq_down_test() ->
+    Seq = range:seq(1, -2, -1),
+    {1, N1} = range:next(Seq),
+    {0, N2} = range:next(N1),
+    {-1, N3} = range:next(N2),
+    {-2, N4} = range:next(N3),
+    ?assertEqual({down, -3, -2, -1}, N4),
+    ?assertEqual(done, range:next(N4)).
+
+seq_step_test() ->
+    Seq = range:seq(0, 4, 2),
+    {0, N1} = range:next(Seq),
+    {2, N2} = range:next(N1),
+    {4, N3} = range:next(N2),
+    ?assertEqual({up, 6, 4, 2}, N3),
+    ?assertEqual(done, range:next(N3)).
+
+seq_step_over_test() ->
+    Seq = range:seq(0, 5, 2),
+    {0, N1} = range:next(Seq),
+    {2, N2} = range:next(N1),
+    {4, N3} = range:next(N2),
+    ?assertEqual(done, range:next(N3)).
+
+seq_step_under_test() ->
+    Seq = range:seq(0, 3, 2),
+    {0, N1} = range:next(Seq),
+    {2, N2} = range:next(N1),
+    ?assertEqual(done, range:next(N2)).
+
+seq_step_float_up_test() ->
+    Seq = range:seq(1, 1.4, 0.2),
+    {1, N1} = range:next(Seq),            % remember we started this is integer 1
+    {1.2, N2} = range:next(N1),
+    {1.4, N3} = range:next(N2),
+    ?assertEqual(done, range:next(N3)).
+
+seq_step_float_down_test() ->
+    Seq = range:seq(1.4, 1, -0.2),
+    {1.4, N1} = range:next(Seq),
+    {1.2, N2} = range:next(N1),
+    {1.0, N3} = range:next(N2),           % notice this is now float
+    ?assertEqual(done, range:next(N3)).
+
+seq_next_silly_test() ->
+    ?assertThrow(range_seq_exhausted, range:next(done)).
+
+upto_test() ->
+    Seq = range:upto(2),
+    {0, N1} = range:next(Seq),
+    {1, N2} = range:next(N1),
+    ?assertEqual(done, range:next(N2)).
+
+countdown_test() ->
+    Seq = range:countdown(2),
+    {2, N1} = range:next(Seq),
+    {1, N2} = range:next(N1),
+    ?assertEqual(done, range:next(N2)).
+
+forever_test() ->
+    Seq = range:forever(2),
+    {2, N1} = range:next(Seq),
+    {3, N2} = range:next(N1),
+    ?assertEqual({4, {forever, 5}}, range:next(N2)).
+
+cycle_test() ->
+    Seq = range:seq(1,3),
+    Cycle = range:cycle(Seq),
+    {1, A1} = range:next(Cycle),
+    {2, A2} = range:next(A1),
+    {3, A3} = range:next(A2),
+    {1, A4} = range:next(A3),
+    {2, A5} = range:next(A4),
+    {3, _} = range:next(A5).
+
+cycle_invalid_test() ->
+    Seq = range:forever(),
+    ?assertThrow(range_cycle_invalid, range:cycle(Seq)).
+
+    
+
+% how do you use iterator in List comp?   You can't, must be a list!!
